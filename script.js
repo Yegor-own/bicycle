cfe912b1d543b2feda14ce9348be5ac563904393
Yegor-own <egor891712@gmail.com>
2022-02-09 19:55:33 +0500

    add new section twitter

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
 		$('#burger,.nav__body').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+
+	$('.twitter__tab:first-child').show();
+  	$('.actions__item').click(function(event) {
+      event.preventDefault();
+      $(this).toggleClass('active_tab').siblings().removeClass('active_tab');
+      var id = $(this).attr('href');
+      $(id).fadeIn().siblings().fadeOut();
+  	});
 });
